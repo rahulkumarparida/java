082035db7b5cef83c544c205c8fa0cc2a2678449
Rahul Kumar Parida <rahulkumarparida52@gmail.com>
2024-08-17 14:18:59 +0000

    added pattern and function

---
+++ java/function.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+//lets learn methods/functions
+class function {
+    //functioin without parameters
+    public static void printhello(){
+        System.out.println("Hello World");
+        System.out.println("This is my first Function");
+    }
+    //function with parameters
+    public static void main(String[] args) {
+        System.out.println("Learning java 17 Aug");
+        printhello();
+    }
+}
---
+++ java/pattern.java
@@ -0,0 +1,104 @@
+class pattern {
+    public static void main(String[] args) {
+        System.out.println("Learning Java 17 Aug 2024");
+        System.out.println("The Pattern is.......");
+//         int a=5;
+//         for(int b=0;b<=a;b++){
+//                 System.out.println("****");
+// /* 
+// ****
+// ****
+// ****
+// ****
+// ****
+// **** */
+//         }
+// }
+            // int c=5;
+            // for(int d=1;d<=c;d++){
+            //     for(int e=1;e<=d;e++){
+            //         System.out.print(d);
+            //     }
+            //     System.out.println("");
+            // }
+/* *
+**
+***
+****
+// ***** */
+
+                    // int a=1;
+                    // for(int i=5;i>=a;i--){
+                    //     for(int j=a;j<=i;j++){
+                    //         System.out.print(j);
+                    //     }
+                    //    System.out.println("");
+                    // }
+// 12345
+// 1234
+// 123
+// 12
+// 1                
+                    // int c=5;
+                    // char f='A';
+                    // for(int d=1;d<=c;d++){
+                    //        for(int e=1;e<=d;e++){
+                    //             System.out.print(f);
+                    //             f++;
+                    //        }
+                    // System.out.println("");
+                    // }
+
+// A
+// BC
+// DEF
+// GHIJ
+// KLMNO
+
+
+
+
+
+//                             int c=5;
+//                     double f= 1.90;
+//                     for(int i=0;i<=c;i++)
+//                     {
+//                         for(int j=0;j<=i;j++)
+//                         {
+//                             System.out.print(f+" ");
+//                             f++;
+//                         }
+//                         System.out.println("");
+//                     }
+
+// 1.9 
+// 2.9 3.9 
+// 4.9 5.9 6.9 
+// 7.9 8.9 9.9 10.9 
+// 11.9 12.9 13.9 14.9 15.9 
+// 16.9 17.9 18.9 19.9 20.9 21.9 
+
+    //                  int c=5;
+    //                 String f= "Rahul";
+    //                 for(int i=0;i<=c;i++)
+    //                 {
+    //                     for(int j=0;j<=i;j++)
+    //                     {
+    //                         System.out.print(f+" ");
+                        
+    //                     }
+    //                     System.out.println(""); 
+    // }
+// Rahul
+// Rahul Rahul 
+// Rahul Rahul Rahul 
+// Rahul Rahul Rahul Rahul 
+// Rahul Rahul Rahul Rahul Rahul 
+// Rahul Rahul Rahul Rahul Rahul Rahul 
+                              
+                   
+
+        }
+
+
+    }
