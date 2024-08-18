76891edbd0b23f76fe86a1fd4123e78a3eb0992d
Rahul Kumar Parida <rahulkumarparida52@gmail.com>
2024-08-18 03:06:02 +0000

    added

---
+++ java/boilercode.java
@@ -0,0 +1,8 @@
+import java.io.*;
+import java.util.*;
+class boilercode {
+    public static void main(String[] args) {
+        System.out.println("Learning Java DATE");
+        System.out.println("Basic Syntax of java");
+    }
+}
