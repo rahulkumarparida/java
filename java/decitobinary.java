5a887cbc5ce320cc441cc7f19a435dcbdc7b5e32
Rahul Kumar Parida <rahulkumarparida52@gmail.com>
2024-08-19 15:32:03 +0000

    nothing new

--- java/decitobinary.java
+++ java/decitobinary.java
@@ -2,19 +2,26 @@ import java.io.*;
 import java.util.*;
 class decitobinary {
     public static void decitobinum(int decinum){
-      int  quotient=2;
-      int binum=1;
-      while( binum > 0){
-        int remiander = decinum%quotient;
-        
-        decinum=decinum/quotient;
-        System.out.print(remiander+" ");
-      
-        binum=decinum;
+      // int  quotient=2;
+      // int binum=1;
+      // while( binum > 0){
+      //   int remiander = decinum%quotient;
+      //   decinum=decinum/quotient;
+      //   System.out.print(remiander+" ");
+      //   binum=decinum;
+            int pow=0;
+            int binum=0;
+            while(decinum>0){
+              int remiander = binum%2;
+              binum=bninum+(remiander*(int)Math.pow(10,pow));
+              pow++;
+
+            }
+        System.out.println(remiander+" ");
               }
-    }
+    
     public static void main(String[] args) {
-        System.out.println("Learning Java 18 Aug 2024");
+        System.out.println("Learning Java 19 Aug 2024");
         decitobinum(4);
         System.out.println("");
     }
