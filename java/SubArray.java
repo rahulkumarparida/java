770ae1a1d2a7d5fd23773e522d2f252494e79bce
Rahul Kumar Parida <rahulkumarparida52@gmail.com>
2024-08-31 02:53:29 +0000

    addedd

--- java/SubArray.java
+++ java/SubArray.java
@@ -1,44 +1,95 @@
 public class SubArray {
     public static void subarray(int num[]){
-
+        
         int count= 0;
-        int n = 0;
-        int total = 0;
-        int TotalofSubarrays[]={};
+        
+        int Maxsum = Integer.MIN_VALUE;
+        int Minsum = Integer.MAX_VALUE;
+        // int TotalofSubarrays[]={};
         int sum = 0;
         for (int i = 0; i < num.length; i++) {
             for(int j= i ; j< num.length ; j++){
+                sum=0;
                  for (int k = i; k<= j ; k++) {
 
-                     System.out.print(num[k]+" " );
-                    
+                    //  System.out.print(num[k]+" " );
+                     sum += num[k];    
                  }
                  count++;
                  //sum of sub arrays
-                 sum += num[j];
-                System.out.print("  " + sum);
-                total = sum;
                 
-                 System.out.print(" , ");
-             
+                System.out.print("  " + sum);
+                if(Maxsum < sum){
+                    Maxsum=sum;
+                }
+                if(Minsum > sum){
+                    Minsum = sum;
+                }
+        
+                // total.add()=TotalofSubarrays[];
+                //  System.out.print(" , ");
+               
                
 
             }
-            sum=0;
+          
             System.out.println(); 
          
             
         }
-        System.out.println( count +" are the total numbers of subarrays");
-       for (int i = 0; i < TotalofSubarrays.length; i++) {
+        System.out.println("Max NUmmber is " + Maxsum);
+        System.out.println("Min Nummber is " + Minsum);
+       System.out.println("Total Sub arrays is  "+ count);
+       
+      
+
+    }
+     public static void prefixsubarray(int num[]){
+        int Maxsum = Integer.MIN_VALUE;
+        int Minsum = Integer.MAX_VALUE;
+     int count= 0;
+     int arr[]=new int[num.length];
+     int sum = 0;
+     arr[0] = num[0];
+     for (int i = 1; i < arr.length; i++) {
+        arr[i] = arr[i-1]+num[i];
+     }
+     
+     for (int i = 0; i < num.length; i++) {
+         for(int j= i ; j< num.length ; j++){
+             sum = i == 0 ? arr[j] : arr[j] - arr[i-1]; 
+              
+              count++;
+              if(Maxsum < sum){
+                Maxsum=sum;
+            }
+            if(Minsum > sum){
+                Minsum = sum;
+            }
+             System.out.print("  " + sum);
             
-        TotalofSubarrays[n]=sum;
-        System.out.println(TotalofSubarrays[n]);
-       }
+               
+             
+            
+            
+
+         }
+       
+         System.out.println(); 
+      
+         
+     }
+     System.out.println("Max NUmmber is " + Maxsum);
+     System.out.println("Min Nummber is " + Minsum);
+    System.out.println("Total Sub arrays is  "+ count);
+    
+   
+
 
     }
     public static void main(String[] args) {
         int num[]= {2,4,6,8,10};
         subarray(num);
+        prefixsubarray(num);
     }
 }
