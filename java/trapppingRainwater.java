64bb52d4ec3bad41878b2a138191033eb57c2e22
Rahul Kumar Parida <rahulkumarparida52@gmail.com>
2024-09-04 02:58:05 +0000

    trying dsa med question

---
+++ java/trapppingRainwater.java
@@ -0,0 +1,19 @@
+public class trapppingRainwater {
+    public static void trappedRainwater(int buildings[]){
+     System.out.println(buildings);
+       int MaxNumLeft[]={};
+       int MaxNumRight[]={};
+       int i =0;
+       for ( i = 0; i < buildings.length; i++) {
+          if (buildings[i]>buildings[i+1]) {
+            buildings[i] = MaxNumLeft[i];
+            MaxNumLeft[i]++;
+          }
+       }
+       System.out.println(MaxNumLeft[i]);
+    }
+    public static void main(String[] args) {
+        int buildings[]={4,2,0,6,3,2,5};
+        trappedRainwater(buildings);
+    }
+}
