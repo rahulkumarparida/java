4bcf34394cb32d1d50864afea69985bbd316592d
Rahul Kumar Parida <rahulkumarparida52@gmail.com>
2024-09-05 02:48:02 +0000

    added

--- java/trapppingRainwater.java
+++ java/trapppingRainwater.java
@@ -1,19 +1,31 @@
 public class trapppingRainwater {
-    public static void trappedRainwater(int buildings[]){
+    public static int  trappedRainwater(int buildings[]){
      System.out.println(buildings);
-       int MaxNumLeft[]={};
-       int MaxNumRight[]={};
-       int i =0;
-       for ( i = 0; i < buildings.length; i++) {
-          if (buildings[i]>buildings[i+1]) {
-            buildings[i] = MaxNumLeft[i];
-           
-          }
+     // maximum number in left
+       int MaxNumLeft[]=new int[buildings.length];
+       MaxNumLeft[0]= buildings[0];
+       for (int i = 1; i < buildings.length; i++) {
+        MaxNumLeft[i]=Math.max(buildings[i], MaxNumLeft[i-1]);
+        
+      }
+      // maximum number in right
+       int MaxNumRight[]=new int[buildings.length];
+       MaxNumRight[buildings.length-1] = buildings[buildings.length -1 ];     
+       for (int i = buildings.length-2; i >= 0; i--) {
+        MaxNumRight[i] = Math.max(buildings[i], MaxNumRight[i+1]);
+
        }
-       System.out.println(MaxNumLeft[i]);
+       // compare the values and fond the answer
+       int count = 0;
+       for (int i = 0; i < buildings.length; i++) {
+         int waterlevel = Math.min(MaxNumLeft[i], MaxNumRight[i]);
+         count += waterlevel - buildings[i];
+       }
+      
+      return count;
     }
     public static void main(String[] args) {
         int buildings[]={4,2,0,6,3,2,5};
-        trappedRainwater(buildings);
+        System.out.println(trappedRainwater(buildings));
     }
 }
